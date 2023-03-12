ea10246a4136c8a8391b08e16f4e5f29cdeb6d02
Pavel <krava2007_87@mail.ru>
2023-03-12 14:18:31 +1000

    finish task

--- task29/Program.cs
+++ task29/Program.cs
@@ -6,6 +6,7 @@
 
 int[] array = { 1, 2, 5, 7, 19, 6, 1, 33 };
 
+
 void PrintArray(int[] arr)
 {
     Console.Write("[");
@@ -13,10 +14,12 @@ void PrintArray(int[] arr)
     int count = arr.Length;
     for (int i = 0; i < count; i++)
     {
-        Console.Write($"{arr[i]} ");
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
     }
     Console.Write("]");
 }
+
 PrintArray(array);
 
 // не знаю как сделать запятые после каждой цифры в массиве при выводе в консоль
